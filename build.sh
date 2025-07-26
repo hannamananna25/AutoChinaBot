63fe99b07d640797bf336c4b52446eb68832fead
hannamananna25 <t773743@gmail.com>
2025-07-27 02:51:16 +1000

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o errexit
+
+# Устанавливаем системные зависимости
+apt-get update
+apt-get install -y libxml2-dev libxslt-dev python3-dev
+
+# Устанавливаем Python-зависимости
+pip install --no-cache-dir -r requirements.txt
