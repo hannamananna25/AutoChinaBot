f47f116e018873f67b88923ffc10895d4abb3044
hannamananna25 <t773743@gmail.com>
2025-07-27 15:21:18 +1000

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-# Устанавливаем системные зависимости
-apt-get update
-apt-get install -y libxml2-dev libxslt-dev python3-dev
-
-# Устанавливаем Python-зависимости
-pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
