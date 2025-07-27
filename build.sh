b9c2501f9d05721f4ef1a9a4581de0e8445eb916
hannamananna25 <t773743@gmail.com>
2025-07-27 14:30:05 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ apt-get update
 apt-get install -y libxml2-dev libxslt-dev python3-dev
 
 # Устанавливаем Python-зависимости
-pip install --no-cache-dir -r requirements.txt
+pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
